90c813fa56192e108e14d515d37f93e2eaef84fd
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-29 20:05:20 +0530

    Add authentication routes for user registration

---
+++ BASIC_PROJECT/BACKEND/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+
+
+
+const express = require("express")
+const router = express.Router()
+
+const authController = require('../controllers/auth.controller')
+
+
+router.get('/register', authController.registerUser)
+
+
+module.exports = router
