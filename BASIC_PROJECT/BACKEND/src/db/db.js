d19c05ca8a4ca256a63231177b0aa46ee85f1a8f
Abhay Pratap Singh <abhay.pratap_cs23@gla.ac.in>
2026-05-29 20:02:43 +0530

    Add database connection module with mongoose

---
+++ BASIC_PROJECT/BACKEND/src/db/db.js
@@ -0,0 +1,18 @@
+
+
+const mongoose = require("mongoose")
+
+
+async function connectDB() {
+    
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to database")
+    }
+    catch(err){
+        console.log("Error while connecting to database", err)
+    }
+}
+
+
+module.exports = connectDB
