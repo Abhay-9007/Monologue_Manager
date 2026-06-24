51b275be87ee64bc8f870472c4a6d70b9e1785ae
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-06-24 21:12:02 +0530

    Add files via upload

--- ADVANCE_PROJECT/BACKEND/server.js
+++ ADVANCE_PROJECT/BACKEND/server.js
@@ -1,4 +1,15 @@
 
-new schema
-and nwe CSS,
-with personalised features.
+
+require('dotenv').config()
+const app = require('./src/app')
+const connectDB = require('./src/db/db')
+
+connectDB()
+
+
+
+
+app.listen(3000, (req, res) => {
+    console.log("Server is Runnign on Port 3000")
+})
+
