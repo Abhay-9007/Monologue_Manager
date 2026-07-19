114f75d025373f1c49e7821f0aa50d87ea85a69d
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-07-19 23:46:38 +0530

    Update app.js

--- ADVANCE_PROJECT/BACKEND/src/app.js
+++ ADVANCE_PROJECT/BACKEND/src/app.js
@@ -11,7 +11,7 @@ const dataRoutes = require("./routes/data.routes")
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json());
