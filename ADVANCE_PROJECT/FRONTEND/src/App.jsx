bb86cd191008a1aa90029c724750ba8cff931ead
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-09-12 21:32:48 +0530

    Add files via upload

--- ADVANCE_PROJECT/FRONTEND/src/App.jsx
+++ ADVANCE_PROJECT/FRONTEND/src/App.jsx
@@ -4,6 +4,8 @@ import Register from "./components/Register.jsx"
 import Login from "./components/Login.jsx"
 import Data from "./components/Data.jsx"
 import Create from "./components/Create.jsx"
+import Home from "./components/Home.jsx"
+import AuthSuccess from "./components/AuthSuccess.jsx"
 
 function App() {
   return (
@@ -14,10 +16,12 @@ function App() {
 
 
     <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/data" element={<Data/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/api/auth/register" element={<Register/>}/>
         <Route path="/create" element={<Create/>}/>
+        <Route path="/auth-success" element={<AuthSuccess/>}/>
     </Routes>
 
     </>
